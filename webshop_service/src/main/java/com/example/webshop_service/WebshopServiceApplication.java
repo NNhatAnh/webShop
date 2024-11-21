105078834e2942ba266ababee998fb4601bc227e
NguyenNhatAnh <anhnhatnguyen2103@gmail.com>
2024-11-22 00:36:41 +0700

    Add back end source

---
+++ webshop_service/src/main/java/com/example/webshop_service/WebshopServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.webshop_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopServiceApplication.class, args);
+	}
+
+}
---
+++ webshop_service/src/test/java/com/example/webshop_service/WebshopServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webshop_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
