0149ab4fbb937ffaf8609b9a2b77740b20761491
NguyenNhatAnh <anhnhatnguyen2103@gmail.com>
2024-12-07 01:56:11 +0700

    delete package

--- webshop_service/src/main/java/com/example/webshop_service/repository/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.webshop_service.repository;
-
-import com.example.webshop_service.module.ProductModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepo extends JpaRepository<ProductModel, Integer> {
-}
