ea0b18dbd8752a546f8ac74ec9a7657b3ec93469
NguyenNhatAnh <anhnhatnguyen2103@gmail.com>
2024-12-25 16:10:58 +0700

    Home fragment

--- Rolex/app/src/main/java/com/example/rolex/MainActivity.java
+++ Rolex/app/src/main/java/com/example/rolex/MainActivity.java
@@ -1,25 +1,29 @@
 package com.example.rolex;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
+import android.view.View;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.example.rolex.adapter.HomeAdapter;
 import com.example.rolex.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    private ViewPager2 viewPager2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,18 +33,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null)
-                        .setAnchorView(R.id.fab).show();
-            }
-        });
+
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_product, R.id.nav_policy, R.id.nav_contact)
                 .setOpenableLayout(drawer)
@@ -48,11 +43,63 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        // Listen for navigation changes to determine if we're on the home page
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            if (destination.getId() == R.id.nav_home) {
+                setupSlider(); // Set up the slider if we are on the home page
+            } else {
+                // If we leave the home page, make sure to hide the slider
+                hideSlider();
+            }
+        });
     }
+
+    private void setupSlider() {
+        // Only setup the slider if it hasn't been set up yet
+        if (viewPager2 == null) {
+            viewPager2 = findViewById(R.id.slider);
+
+            List<Integer> imgList = Arrays.asList(
+                    R.drawable.slide1,
+                    R.drawable.slide2,
+                    R.drawable.slide3
+            );
+
+            HomeAdapter homeAdapter = new HomeAdapter(this, imgList);
+            viewPager2.setAdapter(homeAdapter);
+
+            autoSlide();
+        }
+
+        // Ensure slider is visible on the home page
+        if (viewPager2 != null) {
+            viewPager2.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void hideSlider() {
+        // Make sure to hide the slider when navigating away from the home page
+        if (viewPager2 != null) {
+            viewPager2.setVisibility(View.GONE);
+        }
+    }
+
+    private void autoSlide() {
+        final int delayMillis = 5000;
+        viewPager2.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                int nextItem = (viewPager2.getCurrentItem() + 1) % 3;
+                viewPager2.setCurrentItem(nextItem, true);
+                viewPager2.postDelayed(this, delayMillis);
+            }
+        }, delayMillis);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
